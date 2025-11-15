b7a20c109c8d9c35c4b696d96485c49904b519d3
JoseBerganio <joseberganio@outlook.com>
2025-11-15 11:42:16 +0800

    sdsddsdsd

--- RegistrationForm.cs
+++ RegistrationForm.cs
@@ -1,8 +1,9 @@
 using System.Windows.Forms;
+using System.Drawing.Text;
 
 namespace MyDesktopApp.Modules.Registration{
 
-    partial class RegistrationForm : Register
+    public partial class RegistrationForm : Form
     {
         public RegistrationForm(){
             InitializeComponent();
@@ -13,7 +14,25 @@ namespace MyDesktopApp.Modules.Registration{
             string lastName = txtLastName.Text;
             string email = txtEmail.Text;
             string password = txtPassword.Text;
+            bool areAllFilled = firstName == null && lastName == null && email==null && password==null;
+            bool isChkBoxFilled = chkAcceptTerms.Checked;
 
+            if (areAllFilled && isChkBoxFilled){
+                MessageBox.Show("All fields must be filled.");
+            }
+            else{
+                MessageBox.Show("Hi, "+ firstName +". Thank you for the registration.");
+            }
         }
+
+
+        public void clearInputs(object? sender, EventArgs args){
+            txtFirstName = null;
+            txtLastName = null;
+            txtEmail = null;
+            txtPassword = null;
+        }
+        
     }
 }
+
